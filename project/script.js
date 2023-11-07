e5e0cd0e12358dd03f590bc82881334b39840c34
mayurm88057 <mayurmogre97@gmail.com>
2023-11-07 18:16:04 +0530

    Index

--- project/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".thumbnail").on("click", function() {
-        var imgSrc = $(this).attr("src");
-        $(".full-image").fadeOut(300, function() {
-            $(this).attr("src", imgSrc).fadeIn(300);
-        });
-    });
-});
